f765f68e3e228726467aac103ff9208b3963dd5c
Yan Hong <b612af@gmail.com>
2022-04-01 21:40:20 +0900

    {practise}

---
+++ Truefalse.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Truefalse {
+	 public static void main(String[] args) {
+
+	        int num = 0;
+	        if (num < 1) {
+	        System.out.println("引数が0なら偽。引数が1なら真");
+	    }
+	 }
+}
