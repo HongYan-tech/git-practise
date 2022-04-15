c9dbbf3e2c50e0b8016edda86f911fbafaf80a87
HongYan-tech <99378150+HongYan-tech@users.noreply.github.com>
2022-04-15 22:05:43 +0900

    Update Truefalse.java

--- Truefalse.java
+++ Truefalse.java
@@ -2,7 +2,9 @@ package test;
 
 public class Truefalse {
 	 public static void main(String[] args) {
-
+		 
+　　　　　　　　　Random Random = new Random();
+		int num = Random.nextInt(10);　　　　
 	        int num = 0;
 	        if (num < 1) {
 	        System.out.println("引数が0なら偽。引数が1なら真");
